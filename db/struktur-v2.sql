a20b69418e7145eb42cf3e86e6174fb4c07c67de
leomastakusuma <leomastakusuma@gmail.com>
2014-10-08 10:13:00 +0700

    sql

---
+++ db/struktur-v2.sql
@@ -0,0 +1,166 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.6deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 08, 2014 at 10:09 AM
+-- Server version: 5.5.37-0ubuntu0.13.10.1
+-- PHP Version: 5.5.3-1ubuntu2.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `wisata`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `armada`
+--
+
+CREATE TABLE IF NOT EXISTS `armada` (
+  `id_armada` int(4) NOT NULL AUTO_INCREMENT,
+  `armada` varchar(100) NOT NULL,
+  `gambar` text NOT NULL,
+  `link` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_armada`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `berita`
+--
+
+CREATE TABLE IF NOT EXISTS `berita` (
+  `id_berita` int(3) NOT NULL AUTO_INCREMENT,
+  `id_user` int(4) NOT NULL,
+  `tgl_berita` datetime NOT NULL,
+  `judul_berita` varchar(100) NOT NULL,
+  `isi_berita` text NOT NULL,
+  `gambar` text NOT NULL,
+  PRIMARY KEY (`id_berita`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=34 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bukutamu`
+--
+
+CREATE TABLE IF NOT EXISTS `bukutamu` (
+  `id_bukutamu` int(4) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(50) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `komentar` text NOT NULL,
+  `tanggal` datetime NOT NULL,
+  PRIMARY KEY (`id_bukutamu`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `galery`
+--
+
+CREATE TABLE IF NOT EXISTS `galery` (
+  `id_galery` int(4) NOT NULL AUTO_INCREMENT,
+  `tgl_create` datetime NOT NULL,
+  `judul` varchar(100) NOT NULL,
+  `event` text NOT NULL,
+  `gambar` text NOT NULL,
+  PRIMARY KEY (`id_galery`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kota_wisata`
+--
+
+CREATE TABLE IF NOT EXISTS `kota_wisata` (
+  `id_kotawisata` int(4) NOT NULL AUTO_INCREMENT,
+  `kotawisata` varchar(200) NOT NULL,
+  `gambar` text NOT NULL,
+  `link` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_kotawisata`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registrasi`
+--
+
+CREATE TABLE IF NOT EXISTS `registrasi` (
+  `id_registrasi` int(4) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(200) NOT NULL,
+  `alamat` text NOT NULL,
+  `notelp` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(10) NOT NULL,
+  `tgl_registrasi` date NOT NULL,
+  PRIMARY KEY (`id_registrasi`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=41 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservasi`
+--
+
+CREATE TABLE IF NOT EXISTS `reservasi` (
+  `id_reservasi` int(4) NOT NULL AUTO_INCREMENT,
+  `tgl_reservasi` datetime NOT NULL,
+  `tujuan` varchar(200) NOT NULL,
+  `durasi` varchar(100) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `include_paket` text NOT NULL,
+  `image` text NOT NULL,
+  PRIMARY KEY (`id_reservasi`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE IF NOT EXISTS `transaksi` (
+  `id_transaksi` int(4) NOT NULL AUTO_INCREMENT,
+  `id_reservasi` int(4) NOT NULL,
+  `id_user` int(4) NOT NULL,
+  `jml_reservasi` int(4) NOT NULL,
+  `total_harga` float NOT NULL,
+  `tgl_pemesanan` date NOT NULL,
+  `status` varchar(6) NOT NULL,
+  `invoice` varchar(10) NOT NULL,
+  PRIMARY KEY (`id_transaksi`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id_user` int(4) NOT NULL AUTO_INCREMENT,
+  `user` varchar(10) NOT NULL,
+  `pass` varchar(10) NOT NULL,
+  `level` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
